a91ce2b2f1bc3654eb5038c3b6d1c65fff4a4d00
John Ramalho <johnramalho@outlook.com>
2024-09-30 22:25:20 -0300

    Lista de reservaas

---
+++ js/js.js
@@ -0,0 +1,9 @@
+document.getElementById("toggleMenu").addEventListener("click", function() {
+    const menu = document.querySelector(".pgMenu");
+    menu.classList.toggle("ativo"); // Adiciona ou remove a classe 'ativo' no clique
+});
+
+document.getElementById("toggleMenu2").addEventListener("click", function() {
+    const menu = document.querySelector(".pgMenu");
+    menu.classList.toggle("ativo"); // Adiciona ou remove a classe 'ativo' no clique
+});
